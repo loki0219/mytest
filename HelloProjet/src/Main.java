d4eb49f1b128430503c5206932a0008a501fb648
loki_98 <a17303700269@gmail.com>
2019-07-26 18:37:33 +0800

    在Main类中添加第二个方法

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -3,7 +3,12 @@ import org.junit.Test;
 public class Main {
 
     @Test
-    public void testHelloWrold() {
+    public void testHelloWrold1() {
         System.out.println("HelloWorld!");
+    }
+
+    @Test
+    public void testHelloWrold2() {
+        System.out.println("HelloWorld! 哈哈哈哈哈");
     }
 }
